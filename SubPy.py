3fb43443996dfd23b8d77846ee2607de32470f69
Nikolai Åsen Magnussen <nikolai@crunchbang>
2014-12-23 13:55:25 +0100

    Cleaned up some variable names, plus commenting

--- SubPy.py
+++ SubPy.py
@@ -1,6 +1,11 @@
 import urllib, zipfile, os, sys, glob
 
-EXT = ['.mp4', '.avi', '.mkv']
+#Specifying formats, and download and subtitle string
+FORMAT = ['.mp4', '.avi', '.mkv']
+DOWNLOAD = '/subtitle/download'
+SUBTITLE = '/subtitles/'
+
+#Specifying variables to be used in the script
 lang = 'english'
 directory = os.getcwd() + '/tmp'
 path = os.getcwd()
@@ -8,16 +13,13 @@ files = glob.glob("*")
 torrentURL = ""
 torrentName = "<No file>"
 for f in files:
-	for e in EXT:
-		if e in f:
+	for extension in FORMAT:
+		if  extension in f:
 			torrentName = f
-			torrentURL = 'http://www.subscene.com/subtitles/release?q=' + f.replace(e, '') + '&r=true'
+			torrentURL = 'http://www.subscene.com/subtitles/release?q=' + f.replace(extension, '') + '&r=true'
 			break
 	if len(torrentURL) != 0:
 		break
-DOWNLOAD = '/subtitle/download'
-SUBTITLE = '/subtitles/'
-
 
 #Function that check whether or not a word is in a list, and return 1 if in. 0 if not
 def wordInList(word, wList):
@@ -25,7 +27,6 @@ def wordInList(word, wList):
 		if w == word:
 			return 1
 	return 0
-
 
 #Remove all unnecessary strings before and after the  table
 def removeTags(siteList):
@@ -55,16 +56,14 @@ def filterURL(siteList, phrase):
 			urlList.append(l[6:-1])
 	return urlList
 
-
 ##### MAIN PART OF SCRIPT #####
 print "Searching for: " + torrentName
 
+#Find and add all valid URL's to a list
 site = urllib.urlopen(torrentURL)
 text = site.read()
 tList = text.split()
 removeTags(tList)
-
-#Add all valid subtitle URL's to a list
 urlList = filterURL(tList, SUBTITLE)
 
 #Find the correct language
@@ -76,16 +75,16 @@ for url in urlList:
 #Get download-URL
 subSite = urllib.urlopen(subURL)
 line = (subSite.read()).split()
-uerrlList = filterURL(line, DOWNLOAD)
-uerrl = 'http://www.subscene.com' + uerrlList[0]
+urlList = filterURL(line, DOWNLOAD)
+url = 'http://www.subscene.com' + urlList[0]
 
 #Handle redirects
-test = urllib.urlopen(uerrl)
-uerrl = test.geturl()
+test = urllib.urlopen(url)
+url = test.geturl()
 
 #Get, unzip and delete
 subT = urllib.URLopener()
-subT.retrieve(uerrl, directory)
+subT.retrieve(url, directory)
 ziptest = zipfile.ZipFile(directory)
 ziptest.extractall(path)
 os.remove(directory)
